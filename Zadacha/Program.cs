aec45ee61b85afac2e3f82a6c44fbdd19acc14f2
211 <lolbesttf@gmail.com>
2022-10-15 23:44:48 +0500

    added reshenie

--- Zadacha/Program.cs
+++ Zadacha/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = {"Hello", "2", "4", "world", "GeekBrains","GB", "41241", "qwe", "-78"};
+string[] array2 = new string[0];
+int k = 0;
+for(int i = 0; i < array.Length; i++)
+{
+    if(array[i].Length <= 3)
+    {
+        Array.Resize(ref array2, array2.Length + 1);
+        array2[k] = array[i];
+        k++;
+    } 
+}
+
